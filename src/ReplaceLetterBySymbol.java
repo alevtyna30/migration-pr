31ca81f8802b058f159f0d78ac7af1977938c553
Alevtyna Parysta <luxor952@gmail.com>
2022-12-06 22:38:17 +0700

    Changing the whole text

--- src/ReplaceLetterBySymbol.java
+++ src/ReplaceLetterBySymbol.java
@@ -1,8 +1,14 @@
 public class ReplaceLetterBySymbol {
     public static void main(String[] args) {
-        String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
-                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
-                "when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+        String text = "Down, down, down. There was nothing else to" +
+                "do, so Alice soon began talking again. “Dinah ’ll" +
+                "miss me very much to-night, I should think !”" +
+                "(Dinah was the cat.) “ I hope they’ll remember" +
+                "her saucer of milk at tea-time. Dinah, my dear!" +
+                "I wish you were down here with me ! There" +
+                "are no mice in the air, I’m afraid, but you" +
+                "might catch a bat, and that’s very like a mouse,q" +
+                "you know. But do cats eat bats, I wonder?”";
         int s = 2;
         char symbol = '!';
         System.out.printf("Before: %s%sAfter: %s", text, System.lineSeparator(), replaceCharBySymbol(text, s, symbol));

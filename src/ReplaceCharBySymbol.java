be1ab9ffac1cb8975c30ae9bbfcc15d6a1228c9e
Alevtyna Parysta <luxor952@gmail.com>
2022-12-11 20:01:28 +0700

    Modifying text - make it shorter

--- src/ReplaceCharBySymbol.java
+++ src/ReplaceCharBySymbol.java
@@ -3,12 +3,7 @@ public class ReplaceCharBySymbol {
         String text = "Alise said: Down, down, down. There was nothing else to" +
                 "do, so Alice soon began talking again. “Dinah ’ll" +
                 "miss me very much to-night, I should think !”" +
-                "(Dinah was the cat.) “ I hope they’ll remember" +
-                "her saucer of milk at tea-time. Dinah, my dear!" +
-                "I wish you were down here with me ! There" +
-                "are no mice in the air, I’m afraid, but you" +
-                "might catch a bat, and that’s very like a mouse,q" +
-                "you know. But do cats eat bats, I wonder?”";
+                "(Dinah was the cat.) “";
         int s = 2;
         char symbol = '!';
         System.out.printf("Before: %s%sAfter: %s", text, System.lineSeparator(), replaceCharBySymbol(text, s, symbol));
